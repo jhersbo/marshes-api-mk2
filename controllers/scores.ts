bce6232b70aa87b83b8e8dc17a7403cb64f78678
jhersbo <jhersbo@gmail.com>
2022-09-22 14:31:42 -0500

    fix

--- controllers/scores.ts
+++ controllers/scores.ts
@@ -1,3 +1,11 @@
 let scoresRouter = require('express').Router()
 
+scoresRouter.get('/', async (req: any, res: any)=>{
+    try{
+        res.send('Get stub')
+    }catch(err){
+        res.json("error message: " + err)
+    }
+})
+
 module.exports = scoresRouter
--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 'use strict';
-import { Model } from 'sequelize'
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
     /**
